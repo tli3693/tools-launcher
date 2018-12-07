824d45c477b69873deddc8fa2bb448e45cfaa31d
Troy <tli3693@gmail.com>
2018-12-07 00:13:26 -0500

    Fixes to build jar with dependencies and new config location

--- src/main/java/com/troy/constants/ModConstants.java
+++ src/main/java/com/troy/constants/ModConstants.java
@@ -1,7 +1,7 @@
 package com.troy.constants;
 
 public class ModConstants {
-	public static final String modConfigLocation = "src/main/resources/mod-config.json";
+	public static final String modConfigLocation = "./mod-config.json";
 	public static final String ahkPath = "C:\\Program Files\\AutoHotkey\\AutoHotkey.exe";
 	public static final String ahkExt = "ahk";
 	public static final String exeExt = "exe";
